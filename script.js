05a5c42d1e79acacd5f5c4dd0a7d40d8a75360f3
KOTekin75 <128240028+KOTekin75@users.noreply.github.com>
2023-03-18 20:14:42 +0500

    Add files via upload
    First commit

---
+++ script.js
@@ -0,0 +1,91 @@
+let page = document.querySelector(".body");
+let themeButton = document.querySelector(".themeButton");
+let condition = true;
+let start = 0;
+let end = 0;
+
+themeButton.onclick = function() {
+    page.classList.toggle("lightTheme");
+    page.classList.toggle("darkTheme");
+};
+
+
+$('.menu_small_icon').click(function(){
+    if (condition) {
+        open()
+    } else {
+        close()
+    }
+});
+
+$('.container').on('touchstart', function(e){
+    start = e.originalEvent.touches[0].pageX;
+});
+$('.container').on('touchend', function(e){
+    end = e.originalEvent.changedTouches[0].pageX;
+    if (start < 50) {
+        if (end - start >= 100 && condition) {
+                open()
+            } else if ( start - end >= 100 && !condition) {
+                close()
+            }
+    } else {
+
+    }
+}); 
+
+function open(){
+    anime({
+        targets: '.menu-small',
+        translateX: ['-100%', '0'],
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+    anime({
+        targets: '.menu_small_icon',
+        rotate: 90,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+    anime({
+        targets: '.stick',
+        rotate: 180,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+    condition = false;
+}
+
+function close(){
+    anime({
+        targets: '.menu-small',
+        translateX: [ '0', '-100%'],
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+    anime({
+        targets: '.menu_small_icon',
+        rotate: 0,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+    anime({
+        targets: '.stick',
+        rotate: 0,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+    condition = true;
+}
